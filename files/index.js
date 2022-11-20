2ea8f0135cf166668b6ca1b300216d0aa004e1a5
Mihai Zecheru <92233684+MihaiZecheru@users.noreply.github.com>
2022-11-19 18:49:41 -0800

    add edit & delete mode, fix tabs, & small changes

--- files/index.js
+++ files/index.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
-import { getFirestore, collection, query, orderBy, limit, getDocs } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+import { getFirestore, collection, query, orderBy, limit, getDocs, deleteDoc, doc, updateDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+import { ref, deleteObject, getStorage } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDQbDmNR165Y9tzAlLfInNSKYv8a9OTLTM",
@@ -52,7 +53,10 @@ function createTableRow(doc, num) {
 
   number.setAttribute("scope", "row");
   number.innerText = num;
+  number.setAttribute("data-num", num);
+  number.setAttribute("data-doc-id", doc.doc_id);
   name.innerText = doc.name.substring(0, doc.name.indexOf("."));
+  name.setAttribute("data-name", name.innerText);
   extension.innerText = doc.type;
   size.innerText = doc.size;
   date.innerText = doc.date.toDate().toLocaleString().replace(/,/g, "");
@@ -67,6 +71,8 @@ function createTableRow(doc, num) {
 
   return { row, copy_uuid, download_uuid };
 }
+
+let MODE = "NONE";
 
 function download(url, filename) {
   const link = document.createElement("a");
@@ -109,7 +115,7 @@ function populateTable(docs) {
       const blob = await fetch(url, {
         method: 'GET',
         headers: {
-          'Origin': 'https://127.0.0.1:5500/files'
+          'Origin': window.location.origin
         }
       }).then((r) => r.blob());
       download_blob(blob, filename);
@@ -173,11 +179,16 @@ async function getCurrentUser() {
 
 getCurrentUser().then(async (user) => {
   populateTable(await getFileData(user.email));
+  const rows = document.querySelectorAll("tbody tr");
+  if (rows.length >= 1 && rows[0].children[1].innerText !== "N/A") {
+    document.getElementById("manipulate-files-icons").style.display = "block";
+  }
 }).catch(async () => {
   if (!auth.currentUser) {
     window.location.href = "/login";
   } else {
     populateTable(await getFileData(auth.currentUser.email));
+    document.getElementById("manipulate-files-icons").style.display = "block";
   }
 });
 
@@ -219,5 +230,161 @@ document.getElementById("search-box").addEventListener("keydown", (e) => {
 
   if (e.key === "Enter") {
     document.getElementById("search-button").click();
+  }
+});
+
+function show_checkboxes() {
+  document.querySelectorAll("th").forEach((e) => {
+    if (e.scope === "col") return;
+    e.innerHTML = `${e.getAttribute("data-num")} <input type="checkbox" class="form-check-input" style="margin-left: .4rem;">`;
+
+    e.querySelector("input").addEventListener("change", (checkbox_event) => { // filename                                                        file extension
+      if (MODE === "delete-files") {
+        document.getElementById("confirm-delete-modal-file-name").innerText = e.parentElement.querySelector("td:nth-child(2)").innerText + "." + e.parentElement.querySelector("td:nth-child(3)").innerText;
+        document.getElementById("confirm-delete-modal").setAttribute("data-checkbox", e.getAttribute("data-num"));
+        document.getElementById("confirm-delete-modal").setAttribute("data-doc-id", e.getAttribute("data-doc-id"));
+        new bootstrap.Modal(document.getElementById("confirm-delete-modal")).show();
+      } else {
+        const nameField = e.parentElement.querySelector("td:nth-child(2)");
+        const name = nameField.getAttribute("data-name");
+        if (!checkbox_event.target.checked) {
+          nameField.innerHTML = name;
+        } else {
+          nameField.innerHTML = `<input type="text" class="form-control" id="${name}-input" placeholder="${name}">`;
+          const nameInput = nameField.querySelector("input")
+          nameInput.value = name;
+          nameInput.focus();
+          nameInput.selectionStart = nameInput.selectionEnd = name.length;
+          nameInput.addEventListener("keydown", (event) => {
+            if (event.key === "Enter") {
+              if (nameInput.value !== "" && nameInput.value !== name) {
+                document.getElementById("confirm-rename-modal").setAttribute("data-checkbox", e.getAttribute("data-num"));
+                document.getElementById("confirm-rename-modal").setAttribute("data-doc-id", e.getAttribute("data-doc-id"));
+                const fileExtension = e.parentElement.querySelector("td:nth-child(3)").innerText;
+                document.getElementById("confirm-rename-modal-file-name").innerText = `${name}.${fileExtension}`;
+                document.getElementById("confirm-rename-modal-new-file-name").innerText = `${nameInput.value}.${fileExtension}`;
+                new bootstrap.Modal(document.getElementById("confirm-rename-modal")).show();
+              }
+            } else if (event.key === "Escape") {
+              nameInput.value = name;
+              nameInput.blur();
+            }
+          });
+        }
+      }
+    });
+  });
+}
+
+function hide_checkboxes() {
+  document.querySelectorAll("th").forEach((e) => {
+    if (e.scope === "col") return;
+    e.innerHTML = e.getAttribute("data-num");
+  });
+}
+
+document.querySelectorAll(".manipulate-files").forEach((e) => {
+  e.addEventListener("click", (e) => {
+    if (e.target.style.transform === "scale(2)") {
+      if (MODE === "edit-files") {
+        // remove input boxes from 2nd column in table
+        document.querySelectorAll("tbody tr td:nth-child(2) input").forEach((e) => {
+          e.parentElement.innerHTML = e.getAttribute("placeholder");
+        });
+      }
+
+      MODE = "NONE";
+      e.target.style.transform = "scale(1.5)";
+      hide_checkboxes();
+      return;
+    } else {
+      MODE = e.target.id;
+      e.target.style.transform = "scale(2.0)";
+      show_checkboxes();
+    }
+
+    if (e.target.id === "edit-files") {
+      const deleteFiles = document.getElementById("delete-files");
+      if (deleteFiles.style.transform === "scale(2)") {
+        deleteFiles.style.transform = "scale(1.5)";
+      }
+    } else {
+      const editFiles = document.getElementById("edit-files");
+      if (editFiles.style.transform ==="scale(2)") {
+        editFiles.style.transform = "scale(1.5)";
+      }
+    }
+  });
+});
+
+
+document.getElementById("delete-button").addEventListener("click", async () => {
+  const checkbox = document.getElementById("confirm-delete-modal").getAttribute("data-checkbox");
+  const doc_id = document.getElementById("confirm-delete-modal").getAttribute("data-doc-id");
+  const _doc = doc(getFirestore(), auth.currentUser.email, doc_id);
+  const data = (await getDoc(_doc)).data();
+  const filename = data.original_name || data.name;
+
+  // delete from firestore
+  deleteDoc(_doc).then(async () => {
+    // delete from table
+    document.querySelector(`tbody tr:nth-child(${checkbox})`).remove();
+
+    // delete from storage
+    await deleteObject(ref(getStorage(), filename));
+  });
+});
+
+document.getElementById("close-delete-modal").addEventListener("click", () => {
+  const number = document.getElementById("confirm-delete-modal").getAttribute("data-checkbox");
+  document.querySelectorAll("th").forEach((e) => {
+    if (e.scope === "col") return;
+    if (e.getAttribute("data-num") === number) {
+      e.querySelector("input").checked = false;
+    }
+  });
+});
+
+document.getElementById("close-rename-modal").addEventListener("click", () => {
+  const number = document.getElementById("confirm-rename-modal").getAttribute("data-checkbox");
+  document.querySelectorAll("th").forEach((e) => {
+    if (e.scope === "col") return;
+    if (e.getAttribute("data-num") === number) {
+      e.querySelector("input").checked = false;
+      const inputBox = e.parentElement.querySelector("td:nth-child(2)");
+      const name = inputBox.getAttribute("data-name");
+      inputBox.innerHTML = name;
+    }
+  });
+});
+
+document.getElementById("rename-button").addEventListener("click", () => {
+  const checkbox = document.getElementById("confirm-rename-modal").getAttribute("data-checkbox");
+  const doc_id = document.getElementById("confirm-rename-modal").getAttribute("data-doc-id");
+  const oldFileName = document.getElementById("confirm-rename-modal-file-name").innerText;
+  const newFileName = document.getElementById("confirm-rename-modal-new-file-name").innerText;
+
+  // rename in firestore
+  updateDoc(doc(getFirestore(), auth.currentUser.email, doc_id), { original_name: oldFileName, name: newFileName }).then(() => {
+    // rename in table
+    const tableRow = document.querySelector(`tbody tr:nth-child(${checkbox})`);
+    tableRow.querySelector("td:nth-child(2)").innerHTML = newFileName;
+    tableRow.querySelector("td:nth-child(2)").setAttribute("data-name", newFileName);
+  });
+  document.getElementById("close-rename-modal").click();
+});
+
+document.body.addEventListener("keydown", (e) => {
+  if (e.ctrlKey && e.shiftKey && e.key === "E") {
+    e.preventDefault();
+    document.getElementById("edit-files").click();
+  } else if (e.ctrlKey && e.shiftKey && e.key === "D") {
+    e.preventDefault();
+    document.getElementById("delete-files").click();
+  }
+  console.log(e.target)
+  if (e.target.tagName === "INPUT" && e.target.type === "checkbox" && e.key === "Enter") {
+    e.preventDefault();
+    e.target.click();
   }
 });
